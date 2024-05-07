552ec88f5e2181668604f9b6c624c3c8ea7baaab
lrioxh <lrioxh@foxmail.com>
2024-05-07 19:16:10 +0800

    feat(#2): 添加基于chrome的动态渲染爬取; 添加默认header; 图像批量保存至images/{date}文件夹; 规范化命名，去除特殊字符;

--- bilibili.py
+++ bilibili.py
@@ -1,45 +1,56 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import requests
-import time
+from datetime import datetime
 import re
-from utils import parser
-from utils import upload_img
+from utils import Crawler
 from utils import html2markdown
-from utils import generator
 import sys
 
 
-def get_meta(url):
+def extract_image_url(html_content):
+    # 定义正则表达式模式
+    pattern = r'url\("?(.*?)(?:@|$)'
+    # 使用 findall 函数匹配所有 URL
+    urls = re.findall(pattern, html_content)
+    return f"https:{urls[0]}" if urls else None
+
+def get_meta(spider):
     meta = {}
-    r = parser(url)
-    meta['title'] = r.find('h1', class_='title').text
+    r = spider.dynamic_parser(r'E:\software\common\webBrowser\chrome\chromedriver.exe') #头图为动态渲染
+    # r = spider.static_parser()  
+    meta['title'] = r.find('h1', class_='title').text.strip()
     tag = r'\[.*?\]|【.*?】'  # 去除【】[] 包裹的内容
     meta['title'] = re.sub(tag, '', meta['title'])
-    meta['author'] = r.find(class_='up-name').text
-    meta['original'] = url
-    return meta
-
-def get_date(url):
-    date = parser(url).find(class_='create-time')['data-ts']
-    date = time.localtime(int(date))
-    date = time.strftime('%Y-%m-%d', date)
-    return date
-
-def get_posts(url):
-    r = parser(url)
-    post = str(r.find(class_='article-holder'))
+    meta['author'] = r.find(class_='up-name').text.strip()
+    meta['original'] = spider.url
+    # date
+    date_string = r.find(class_='publish-text').text
+    # 解析为 datetime 对象
+    date_obj = datetime.strptime(date_string, '%Y年%m月%d日 %H:%M')
+    date = date_obj.strftime('%Y-%m-%d')
+    # post
+    post = str(r.find(class_='article-content'))
     post = post.replace('data-src', 'src')
-    img_src = r"""\bsrc\b\s*=\s*[\'\"]?([^\'\"]*)[\'\"]?"""
+    img_src = r"""<img\b[^>]*\bsrc\s*=\s*['"]([^'"]*)['"][^>]*>"""
+    # img_src = r"""<img\b[^>]*\bsrc\s*=\s*['"]([^'"@]*)['"@][^>]*[>]"""    #匹配到@为止
     for img in re.findall(img_src, post):
-        new_img = upload_img(img)
+        new_img=f"https:{img.split('@')[0]}"
+        new_img = spider.upload_img(new_img,date)
         post = post.replace(img, new_img)
+    banner = str(r.find(class_='banner-container'))
+    head_img = extract_image_url(banner)
+    if head_img:
+        meta['header-img'] = spider.upload_img(head_img,date)
     post = html2markdown(post)
-    return post
+    return meta,date,post
 
-def bilibili_spider(id):
-    url = f'https://www.bilibili.com/read/cv{id}'
-    generator('bilibili', get_meta(url), get_posts(url), get_date(url))
+def bilibili_spider(ids):
+    for id in ids:
+        if id == '':
+            continue
+        url = f'https://www.bilibili.com/read/cv{id}'
+        spider = Crawler(url)
+        spider.generator('bilibili', *get_meta(spider))
 
 if __name__ == '__main__':
-    bilibili_spider(str(sys.argv[1]))
+    bilibili_spider(sys.argv[1:])
--- utils.py
+++ utils.py
@@ -4,19 +4,70 @@
 ' 上传图片、解析器等 '
 
 import requests
-import os
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import os,random
 from io import BytesIO
 from bs4 import BeautifulSoup
 from html2text import html2text
 import re
 
+USER_AGENT_LIST = [
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
+    "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; 360SE)",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36",
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE'
+]
 
-def parser(url, headers=None):
-    content = requests.get(url, headers=headers, timeout=10).content
+default_headers = {
+            'User-Agent':random.choice(USER_AGENT_LIST)
+        }
+
+def clean_chars(text):
+    # 定义全角标点与对应的半角标点的映射关系
+    punctuation_mapping = {
+        '，': ',',
+        '。': '.',
+        '！': '!',
+        '？': '?',
+        '‘': '\'',
+        '’': '\'',
+        '（': '(',
+        '）': ')',
+        '：': ':',
+    }
+    
+    special_chars = r'[<>:"/\\|?*\x00-\x1F\x7F]'    #替换文件系统的特殊字符
+    
+    pattern = '|'.join(re.escape(p) for p in punctuation_mapping.keys())
+    result = re.sub(pattern, lambda m: punctuation_mapping[m.group()], text)
+    result = re.sub(special_chars, '_', result)
+    return result
+
+
+def html2markdown(text):
+    pattern = re.compile(r'<span.*?>.*?</span>')
+    spans = pattern.findall(text)
+    for index, span in enumerate(spans):
+        text = text.replace(span, f'span{index}')
+    text = html2text(text)
+    for index, span in enumerate(spans[::-1], start=1):
+        text = text.replace(f'span{len(spans) - index}', span)
+    return text
+
+def parser(url,headers=None):
+    content = requests.get(url, headers=headers if headers else default_headers, timeout=10).content
     return BeautifulSoup(content, 'html.parser')
 
-
-def upload_img(url):
+def upload_img(url,date = None):
     print('正在使用 sm.ms 上传图片……')
     try:  # sm.ms API v2
         img = BytesIO(requests.get(url).content)
@@ -33,11 +84,18 @@ def upload_img(url):
     except Exception as e:
         try:
             print(f'使用 sm.ms 上传失败（{e}），使用 GitHub 作为图床……')
+            r = requests.get(url,headers=default_headers)
+            r.raise_for_status()
             filename = url.split('/')[-1]
-            with open(f'../hibikilogy.github.io/images/{filename}', 'wb') as f:
-                f.write(requests.get(url).content)
-            print('上传成功，需提交 hibikilogy.github.io 中的改动方可使用。')
+            dir = f"../hibikilogy.github.io/images/{date}"
+            if not os.path.exists(dir):
+                os.makedirs(dir)
+            with open(f'{dir}/{filename}', 'wb') as f:
+                f.write(r.content)
+            print('本地写入成功，需提交 hibikilogy.github.io 中的改动方可使用。')
             return f'https://cdn.jsdelivr.net/gh/hibikilogy/hibikilogy.github.io/images/{filename}'
+        except requests.exceptions.RequestException as e:
+            print("无法下载图像:", e)
         except FileNotFoundError:
             print(f'上传失败，已使用原链接。请检查是否在上级目录内存在 hibikilogy.github.io 的本地仓库。')
             return url
@@ -45,35 +103,91 @@ def upload_img(url):
             print(f'上传失败（{e}），已使用原链接。')
             return url
 
-
-def html2markdown(text):
-    pattern = re.compile(r'<span.*?>.*?</span>')
-    spans = pattern.findall(text)
-    for index, span in enumerate(spans):
-        text = text.replace(span, f'span{index}')
-    text = html2text(text)
-    for index, span in enumerate(spans[::-1], start=1):
-        text = text.replace(f'span{len(spans) - index}', span)
-    return text
-
-def generator(tag, meta, posts, date):
+def generator(tag, meta, date, posts):
     print('生成文件中……')
     if not os.path.exists('temp'):
         os.makedirs('temp')
-    with open(f'temp/{date}-{meta["title"]}.md', 'w', encoding='utf-8') as f:
+    cleaned_title = clean_chars(meta['title'])
+    with open(f'temp/{date}-{cleaned_title}.md', 'w', encoding='utf-8') as f:
         f.write('---\n')
         f.write('layout: post\n')
         for key in meta:
             f.write(f'{key}: {meta[key]}\n')
-        img = re.compile(r'!\[.*\]\((.*)\)')
-        try:  # 头图
-            header = img.findall(posts)[0]
-            f.write(f'header-img: {header}\n')
-        except IndexError:
-            pass
         f.write('catalog: true\n')
         f.write('tags:\n')
         f.write(f'    - {tag}\n')
         f.write('---\n')
         f.write(posts)
-    print(f'temp/{date}-{meta["title"]}.md已生成。')
+    print(f'temp/{date}-{cleaned_title}.md已生成。')
+
+class Crawler():
+    def __init__(self, url=None, headers=None):
+        self.url = url
+        self.headers = {
+            'User-Agent':random.choice(USER_AGENT_LIST)
+        } if headers==None else headers
+        
+    def static_parser(self, headers=None):
+        content = requests.get(self.url, headers=self.headers, timeout=10).content
+        return BeautifulSoup(content, 'html.parser')
+    
+    def dynamic_parser(self, chrome_exe_path):
+        #需要chromedriver路径,下载：https://googlechromelabs.github.io/chrome-for-testing/#stable
+        service = Service(chrome_exe_path)
+        driver = webdriver.Chrome(service=service)  
+        driver.get(self.url)
+        return BeautifulSoup(driver.page_source, 'html.parser')
+
+    def upload_img(self, url,date = None):
+        print('正在使用 sm.ms 上传图片……')
+        try:  # sm.ms API v2
+            img = BytesIO(requests.get(url).content)
+            body = {'smfile': img}
+            r = requests.post('https://sm.ms/api/v2/upload', data=None, files=body, timeout=10)
+            try:
+                with open('img.txt', 'a') as f:
+                    f.write(f'{r.json()["data"]["url"]}（{r.json()["data"]["delete"]}）\n')
+                print('上传成功，地址和删除链接已写入 img.txt。')
+                return r.json()['data']['url']
+            except KeyError:
+                print('上传成功。')
+                return r.json()['images']
+        except Exception as e:
+            try:
+                print(f'使用 sm.ms 上传失败（{e}），使用 GitHub 作为图床……')
+                r = requests.get(url,headers=self.headers)
+                r.raise_for_status()
+                filename = url.split('/')[-1]
+                dir = f"../hibikilogy.github.io/images/{date}"
+                if not os.path.exists(dir):
+                    os.makedirs(dir)
+                with open(f'{dir}/{filename}', 'wb') as f:
+                    f.write(r.content)
+                print('本地写入成功，需提交 hibikilogy.github.io 中的改动方可使用。')
+                return f'https://cdn.jsdelivr.net/gh/hibikilogy/hibikilogy.github.io/images/{date}/{filename}'
+            except requests.exceptions.RequestException as e:
+                print("无法下载图像:", e)
+            except FileNotFoundError:
+                print(f'上传失败，已使用原链接。请检查是否在上级目录内存在 hibikilogy.github.io 的本地仓库。')
+                return url
+            except Exception as e:
+                print(f'上传失败（{e}），已使用原链接。')
+                return url
+    
+    def generator(self, tag, meta, date, posts):
+        print('生成文件中……')
+        if not os.path.exists('temp'):
+            os.makedirs('temp')
+        cleaned_title = clean_chars(meta['title'])
+        with open(f'temp/{date}-{cleaned_title}.md', 'w', encoding='utf-8') as f:
+            f.write('---\n')
+            f.write('layout: post\n')
+            for key in meta:
+                f.write(f'{key}: {meta[key]}\n')
+            f.write('catalog: true\n')
+            f.write('tags:\n')
+            f.write(f'    - {tag}\n')
+            f.write('---\n')
+            f.write(posts)
+        print(f'temp/{date}-{cleaned_title}.md已生成。')
+
