514791e86ab2fff0becdca46cd5d3eb8ca5eaab2
lrioxh <lrioxh@foxmail.com>
2024-05-07 19:54:34 +0800

    feat: 优先使用原图床而非下载上传

--- bilibili.py
+++ bilibili.py
@@ -8,13 +8,18 @@ import sys
 
 
 def extract_image_url(html_content):
-    # 定义正则表达式模式
-    pattern = r'url\("?(.*?)(?:@|$)'
-    # 使用 findall 函数匹配所有 URL
+    pattern = r'url\("?(.*?)(?:@|$)'    #匹配url("到@的字符
     urls = re.findall(pattern, html_content)
     return f"https:{urls[0]}" if urls else None
 
 def get_meta(spider):
+    # date
+    date_string = r.find(class_='publish-text').text
+    # 解析为 datetime 对象
+    date_obj = datetime.strptime(date_string, '%Y年%m月%d日 %H:%M')
+    date = date_obj.strftime('%Y-%m-%d')
+    
+    # meta
     meta = {}
     r = spider.dynamic_parser(r'E:\software\common\webBrowser\chrome\chromedriver.exe') #头图为动态渲染
     # r = spider.static_parser()  
@@ -23,11 +28,12 @@ def get_meta(spider):
     meta['title'] = re.sub(tag, '', meta['title'])
     meta['author'] = r.find(class_='up-name').text.strip()
     meta['original'] = spider.url
-    # date
-    date_string = r.find(class_='publish-text').text
-    # 解析为 datetime 对象
-    date_obj = datetime.strptime(date_string, '%Y年%m月%d日 %H:%M')
-    date = date_obj.strftime('%Y-%m-%d')
+    banner = str(r.find(class_='banner-container'))
+    head_img = extract_image_url(banner)
+    if head_img:
+        meta['header-img'] = head_img
+        # meta['header-img'] = spider.upload_img(head_img,date)   #直接使用原图床则注释此行
+    
     # post
     post = str(r.find(class_='article-content'))
     post = post.replace('data-src', 'src')
@@ -35,12 +41,8 @@ def get_meta(spider):
     # img_src = r"""<img\b[^>]*\bsrc\s*=\s*['"]([^'"@]*)['"@][^>]*[>]"""    #匹配到@为止
     for img in re.findall(img_src, post):
         new_img=f"https:{img.split('@')[0]}"
-        new_img = spider.upload_img(new_img,date)
+        # new_img = spider.upload_img(new_img,date)   #直接使用原图床则注释此行
         post = post.replace(img, new_img)
-    banner = str(r.find(class_='banner-container'))
-    head_img = extract_image_url(banner)
-    if head_img:
-        meta['header-img'] = spider.upload_img(head_img,date)
     post = html2markdown(post)
     return meta,date,post
 
